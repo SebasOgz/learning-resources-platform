38210e7572cda63b7148fee9d66ea8d7fabde1f2
“Sebastian <ogsebas06@gmail.com>
2023-12-17 17:29:48 -0500

    ADD configuracion de endpoint para mostrar categorias en un selector

--- resources/js/Components/ResourceForm.vue
+++ resources/js/Components/ResourceForm.vue
@@ -1,10 +1,18 @@
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
+let categories = ref([]);
 let title = ref("");
 let description = ref("");
 let link = ref("");
+let category_id = ref(null);
+
+onMounted(() => {
+    axios.get("api/categories").then((response) => {
+        categories.value = response.data;
+    });
+});
 
 function createResource(){
     axios
@@ -22,9 +30,18 @@ function createResource(){
 
 <template>
 <div class="m-8">
+    <label class="p-4">Titulo</label>
     <input type="text" v-model="title">
+    <label class="p-4">Descripción</label>
     <input type="text" v-model="description">
+    <label class="p-4">Link</label>
     <input type="text" v-model="link">
-    <button @click="createResource">Crear Recurso</button>
+    <label class="p-4">Categoría</label>
+    <select v-model="category_id">
+        <option v-for="category in categories" :key="category.id">
+            {{ category.name }}
+        </option>
+    </select>
+    <button @click="createResource" class="m-8 bg-gray-900 text-white">Crear Recurso</button>
 </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ResourceController;
 use App\Models\Category;
@@ -35,5 +36,5 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('api/resources', [ResourceController::class, 'search']);
-
 Route::middleware('auth')->post('api/resources', [ResourceController::class, 'store']);
+Route::middleware('auth')->get('api/categories', [CategoryController::class, 'index']);
